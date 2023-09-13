29a17a9cd1243000bea21802bf278786637eafd4
Ross Anthony R Relator <ross.relator@uconn.edu>
2023-09-13 10:31:44 -0400

    task4 logic

--- app/src/main/java/com/example/hw1_robots/MainActivity.kt
+++ app/src/main/java/com/example/hw1_robots/MainActivity.kt
@@ -3,19 +3,21 @@ package com.example.hw1_robots
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageButton
 import android.widget.ImageView
-import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var redImg : ImageView
     private lateinit var whiteImg : ImageView
     private lateinit var yellowImg : ImageView
-    private lateinit var messageBox : TextView
+
+    private lateinit var rotateCW : ImageButton
+    private lateinit var rotateCCW : ImageButton
 
     private lateinit var robotImages : MutableList<ImageView>
 
-    private var turnCount = 1
+    private var turnCount = 0
     private val robots = listOf( // immutable
         Robot(R.string.red_robot_msg, false,
             R.drawable.king_of_detroit_robot_red_large, R.drawable.king_of_detroit_robot_red_small),
@@ -32,50 +34,31 @@ class MainActivity : AppCompatActivity() {
         redImg = findViewById(R.id.redRobot)
         whiteImg = findViewById(R.id.whiteRobot)
         yellowImg = findViewById(R.id.yellowRobot)
-        messageBox = findViewById(R.id.message_box)
 
         robotImages = mutableListOf(redImg, whiteImg, yellowImg)
 
-        redImg.setOnClickListener{view : View -> toggleImage()}
-        whiteImg.setOnClickListener{view : View -> toggleImage()}
-        yellowImg.setOnClickListener{view : View -> toggleImage()}
+        rotateCW = findViewById(R.id.rot_clockwise)
+        rotateCCW = findViewById(R.id.rot_counter)
+
+        rotateCW.setOnClickListener{view : View -> toggleImage(false)}
+        rotateCCW.setOnClickListener{view : View -> toggleImage(true)}
     }
 
-    private fun toggleImage() {
-        turnCount++
-        if(turnCount > 3) {
-            turnCount = 1
+    private fun toggleImage(isCCW: Boolean) {
+        if(isCCW) {
+            turnCount++
+            if(turnCount > 3) {
+                turnCount = 1
+            }
+        } else {
+            turnCount--
+            if(turnCount <= 0) {
+                turnCount = 3
+            }
         }
 
-//        when (turnCount) {
-//            // setText is better for grabbing resources
-//            0 -> messageBox.setText(R.string.yellow_robot_msg)
-//            1 -> messageBox.setText(R.string.red_robot_msg)
-//            2 -> messageBox.setText(R.string.white_robot_msg)
-//            else -> messageBox.setText("This must be an error")
-//        }
-
-        updateMessageBox()
         setRobotTurn()
         setRobotImages()
-
-//        if (turnCount == 1) { //red is large
-//            redImg.setImageResource(R.drawable.king_of_detroit_robot_red_large)
-//            whiteImg.setImageResource(R.drawable.king_of_detroit_robot_white_small)
-//            yellowImg.setImageResource(R.drawable.king_of_detroit_robot_yellow_small)
-//        } else if (turnCount == 2) {
-//            redImg.setImageResource(R.drawable.king_of_detroit_robot_red_small)
-//            whiteImg.setImageResource(R.drawable.king_of_detroit_robot_white_large)
-//            yellowImg.setImageResource(R.drawable.king_of_detroit_robot_yellow_small)
-//        } else {
-//            redImg.setImageResource(R.drawable.king_of_detroit_robot_red_small)
-//            whiteImg.setImageResource(R.drawable.king_of_detroit_robot_white_small)
-//            yellowImg.setImageResource(R.drawable.king_of_detroit_robot_yellow_large)
-//        }
-    }
-
-    private fun updateMessageBox() {
-        messageBox.setText(robots[turnCount - 1].messageResource)
     }
 
     private fun setRobotTurn() {
